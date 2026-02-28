b128bb9403ff1d3947b1f7f971df7ea67224a9e4
Gerardo Jiménez <gerardojimenez251290@correo.itm.edu.co>
2026-02-28 10:02:12 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,9 +1,8 @@
 from fastapi import FastAPI
 
-
-app= FastAPI()
+app = FastAPI(title="Sistema de reservas de vuelos")
 
 
 @app.get("/")
 def hola():
-    return{"mensaje":"hola mundo"}
+    return {"mensaje": "hola mundo"}
