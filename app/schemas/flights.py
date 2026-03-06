3a28fb46a87612ff44823feed0712adc8b069e99
Angel_7195 <angelgutierrez1132998@correo.itm.edu.co>
2026-03-05 22:18:40 -0500

    schemas de flights y passengers

---
+++ app/schemas/flights.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel
+from uuid import UUID
+from datetime import datetime
+
+class FlightBase(BaseModel):
+    airline_id:UUID
+    flight_number:str
+    origin_airport_id: UUID
+    destination_airport_id: UUID
+    departure_at: datetime
+    arrival_at: datetime
+    status: str = "SCHEDULED"
+
+class FlightUpdate(BaseModel):
+    flight_number: str | None = None
+    departure_at: datetime | None = None
+    arrival_at: datetime | None = None
+    status: str | None = None
+
+class FlightResponse(FlightBase):
+    id: UUID
+    created_at: datetime
+
+    class Config:
+        from_attributes = True
